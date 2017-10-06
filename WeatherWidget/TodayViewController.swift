5f0b902407dd8ae0e5dc31bf1b93c2d4c7d856bb
devxris <chuwei0915@gmail.com>
2017-10-06 13:29:40 +0800

    weatheWidget fetch weather data initially and off-screen

--- WeatherWidget/TodayViewController.swift
+++ WeatherWidget/TodayViewController.swift
@@ -18,9 +18,35 @@ class TodayViewController: UIViewController, NCWidgetProviding {
 	
 	var location = "Paris, France"
 	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		cityLabel.text = location
+		
+		// get weather data from WeatherInfoKit initially
+		WeatherService.shared.getCurrentWeather(at: location) { (data) in
+			DispatchQueue.main.async {
+				guard let temp = data?.main?.temp, let desc = data?.weather?[0].description else { return }
+				self.updateUI(desc: desc, temp: temp)
+			}
+		}
+	}
+	
+	// update widget when it's off-screen
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
 		
+		cityLabel.text = location
+		
+		WeatherService.shared.getCurrentWeather(at: location) { (data) in
+			guard let data = data else { completionHandler(NCUpdateResult.noData); return }
+			guard let temp = data.main?.temp, let desc = data.weather?[0].description else { completionHandler(NCUpdateResult.noData); return }
+			self.updateUI(desc: desc, temp: temp)
+		}
         completionHandler(NCUpdateResult.newData)
     }
-    
+	
+	private func updateUI(desc: String, temp: Double) {
+		weatherLabel.text = desc
+		temperatureLabel.text = String(format: "%d", temp) + "\u{00B0}"
+	}
 }
